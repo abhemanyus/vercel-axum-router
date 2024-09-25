6d993c0452919c5e4c175256c305f6a3e6adfb63
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-09-25 23:11:07 +0530

    add tracing

--- api/main.rs
+++ api/main.rs
@@ -1,8 +1,15 @@
 use axum::{routing::get, Router};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 use vercel_runtime::{process_request, process_response, run_service, Error, ServiceBuilder};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(
+            |_| "vermicelli=debug,tower_sessions=debug,sqlx=warn,tower_http=debug".into(),
+        )))
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()?;
     let app = Router::new().route("/hello", get(root));
     let handler = ServiceBuilder::new()
         .map_request(process_request)
